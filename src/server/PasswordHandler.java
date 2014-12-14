ba1927dcf5891e56f591f39a73f83d7fa8987c7f
Michael <larsen_max@hotmail.com>
2014-12-14 07:41:21 +0100

    lavet password handler del 2

--- src/server/PasswordHandler.java
+++ src/server/PasswordHandler.java
@@ -6,18 +6,26 @@
 package server;
 
 import com.google.gson.Gson;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import java.io.IOException;
 
 /** 
  *
  * @author Michael
  */
-public class PasswordHandler {
+public class PasswordHandler implements HttpHandler{
     private final Facade facade;
     private final Gson gson;
 
     public PasswordHandler() {
         facade = Facade.getFacade(true);
         gson = new Gson();
+    }
+
+    @Override
+    public void handle(HttpExchange he) throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
